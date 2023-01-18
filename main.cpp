1f1e9fcc4bf91089ceee617eed2919a7246cb084
Daniil199820 <47990830+Daniil199820@users.noreply.github.com>
2023-01-18 22:17:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,23 +13,5 @@
 #include "DB_client.hpp"
  
 int main(){
-  /* DB_controller DB;
-    DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD)");
-    DB.process("INSERT INTO table_name VALUES (3)");
-    std::string result = DB.process("SELECT FROM * table_name");
-*/
-    
-
-    DB_data db_data;
-    std::string my_name = "awd";
-    std::string n_col = "n_cold";
-    std::pair<int,std::string> m_p = std::make_pair<int,std::string>(0,"n_col"); 
-    db_data.Create_Table(my_name);
-    db_data.Add_Column(my_name,m_p);
-    std::vector<std::string> vv = {"5"};
-    db_data.Insert_Value(my_name,vv);
-    db_data.Insert_Value(my_name,vv);
-    db_data.Show_Table_Data(my_name);
-
     return 0;
 }
