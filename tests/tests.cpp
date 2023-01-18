b570ec6878ff9e8a4283dc9ff483d71f6aa21b8d
Daniil199820 <47990830+Daniil199820@users.noreply.github.com>
2023-01-18 22:15:55 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,44 +3,57 @@
 
 TEST(Correct_request, Create_Table_1){
     DB_controller DB;
-    std::string result = DB.process("CREATE COLUMN (CUSTOM_INT, AWD)");
+    std::string result = DB.process("CREATE COLUMN (CUSTOM_INT AWD)");
     ASSERT_EQ("Error - unknown <CREATE> method.",result);
 }
 
 TEST(Correct_request, Create_Table_2){
     DB_controller DB;
-    std::string result = DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD)");
+    std::string result = DB.process("CREATE TABLE table_name (CUSTOM_INT AWD)");
     ASSERT_EQ("",result);
 }
 
 TEST(Correct_request, Create_Table_3){
     DB_controller DB;
-    std::string result = DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD");
+    std::string result = DB.process("CREATE TABLE table_name (CUSTOM_INT AWD");
     ASSERT_EQ("No end of arguments - <)>.",result);
 }
 
 TEST(Functional, delete_column_1){
     DB_controller DB;
-    DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD)");
+    DB.process("CREATE TABLE table_name (CUSTOM_INT AWD)");
     std::string result = DB.process("DELETE FROM table_nam");
     ASSERT_EQ("Table doesn't exist.",result);
 }
 
 TEST(Functional, delete_column_2){
     DB_controller DB;
-    DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD)");
+    DB.process("CREATE TABLE table_name (CUSTOM_INT AWD)");
     std::string result = DB.process("DELETE FROM table_name");
     ASSERT_EQ("",result);
 }
 
 TEST(Functional, Insert_value_1){
     DB_controller DB;
-    DB.process("CREATE TABLE table_name (CUSTOM_INT, AWD)");
-    DB.process("INSERT VALUES INTO table_name (3)");
+    DB.process("CREATE TABLE table_name (CUSTOM_INT col_n)");
+    DB.process("INSERT INTO table_name VALUES (3)");
     std::string result = DB.process("SELECT FROM * table_name");
-    ASSERT_EQ("",result);
+    ASSERT_EQ("col_n \n3 \n",result);
 }
 
+TEST(Functional, Insert_value_2){
+    DB_controller DB;
+    DB.process("CREATE TABLE table_name (CUSTOM_INT col_int, CUSTOM_STRING col_string)");
+    DB.process("INSERT INTO table_name VALUES (3,'my_value')");
+    std::string result = DB.process("SELECT FROM * table_name");
+    ASSERT_EQ("col_int col_string \n3 my_value \n",result);
+}
 
-
+TEST(Functional, Insert_value_3){
+    DB_controller DB;
+    DB.process("CREATE TABLE table_name (CUSTOM_INT col_int, CUSTOM_STRING col_string)");
+    DB.process("INSERT INTO table_name VALUES (3,'my_value')");
+    std::string result = DB.process("SELECT FROM * table_another");
+    ASSERT_EQ("Table doesn't exist.",result);
+}
 
